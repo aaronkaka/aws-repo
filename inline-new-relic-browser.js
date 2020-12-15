38d9493cd87b5d9c1495d63bc5d9255863d659a0
Aaron Kaka <aaron.kaka@gmail.com>
2020-12-15 07:16:48 -0500

    Consistently use single quotes.

--- inline-new-relic-browser.js
+++ inline-new-relic-browser.js
@@ -1,12 +1,12 @@
 'use strict';
-// Reference the AWS SDK built into the Lambda execution environment.
+// Import the AWS SDK built into the Lambda execution environment.
 const aws = require('aws-sdk');
-// Reference the S3 API available in the AWS SDK.
+// Import the S3 API available in the AWS SDK.
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
-// Reference the built-in module of Node.js for HTTP data transfer - avoiding the need to package up and deploy a third party library for this purpose.
+// Import the core Node.js module for HTTP data transfer - avoiding the need to package up and deploy a third party library for this purpose.
 const http = require('http');
 // Set the target for fetching the New Relic Browser script.
-const url = "http://js-agent.newrelic.com/nr-loader-spa-current.min.js";
+const url = 'http://js-agent.newrelic.com/nr-loader-spa-current.min.js';
 
 exports.handler = async (event, context) => {
 
@@ -19,6 +19,7 @@ exports.handler = async (event, context) => {
     try {
         // Fetch the index.html from the S3 bucket. We cannot use an S3 Select, as that only applies to objects of CSV, JSON, or Parquet format.
         const s3GetObjectPromise = s3.getObject(s3Params).promise();
+
         // GET the New Relic Browser script from the target CDN.
         const NRPromise = new Promise(function(resolve, reject) {
 
@@ -49,7 +50,7 @@ exports.handler = async (event, context) => {
 
             // Inline the script into the proper position in the HTML.
             let aggregation = inlineIndex > -1 ? html.substring(0, inlineIndex) + NRscript + html.substring(inlineIndex) : '';
-            aggregation = aggregation.replace(/\r?\n|\r/g, " ");
+            aggregation = aggregation.replace(/\r?\n|\r/g, ' ');
 
             return aggregation;
         });
