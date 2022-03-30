79c16e10984f61ac660233a4216b12acd352e6b2
Aaron Kaka <aaron.kaka@gmail.com>
2022-03-30 13:41:39 -0400

    Update reverse-proxy-cloudfront-origin-request-to-api-gateway.js

--- reverse-proxy-cloudfront-origin-request-to-api-gateway.js
+++ reverse-proxy-cloudfront-origin-request-to-api-gateway.js
@@ -2,8 +2,13 @@ exports.handler = async (event, context, callback) => {
     const request = event.Records[0].cf.request;
 
     if (request.uri.includes('/api')) {
-        request.uri = '/';
+        request.uri = request.uri.replace('/api', '');
+        if (request.uri == '') {
+            request.uri = '/';
+        }
+        console.log('modified request uri');
     }
+
     request.headers['x-api-key'] = [{key: 'X-Api-Key', value: 'Slava_Ukraini!'}];
 
     callback(null, request);
