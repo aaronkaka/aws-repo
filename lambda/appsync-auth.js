a10852b567a658995caf5f3155630a63fca7e196
Aaron Kaka <aaron.kaka@gmail.com>
2022-06-29 12:17:54 -0400

    Update appsync-auth.js

--- lambda/appsync-auth.js
+++ lambda/appsync-auth.js
@@ -4,8 +4,7 @@ const jwksClient = require('jwks-rsa');
 const client = jwksClient({
     cache: true, // Default Value
     cacheMaxEntries: 32,
-    //jwksUri: process.env.JWKS_URI + 'jwks.json',
-    jwksUri: 'https://int-piapi-internal.stg-openclass.com/tokens/jwks.json',
+    jwksUri: process.env.JWKS_URI + 'jwks.json',
     requestHeaders: {}, // Optional
     timeout: 30000 // Defaults to 30s
 });
@@ -15,23 +14,21 @@ const verificationOptions = {
 }
 
 exports.handler = async (event) => {
-    let token = event.authorizationToken || '';
-    const header = token.split(".");
+    const userToken = event.authorizationToken || '';
+    const header = userToken.split(".");
     // the secret will be decoded using base64 and the token verification will use the original random bytes
     const jwtHead = Buffer.from(header[0], "base64").toString();
     const kid = JSON.parse(jwtHead).kid;
 
     const key = await client.getSigningKey(kid);
     const signingKey = key.getPublicKey();
-    console.log('kid: ' + kid + '/ ' + signingKey);
-    
     const response = {
         isAuthorized: false,
         resolverContext: JSON.parse(Buffer.from(header[1], "base64").toString())
     };
     
     try {
-        let decoded = jwt.verify(token, signingKey, verificationOptions);
+        let decoded = jwt.verify(userToken, signingKey, verificationOptions);
         response.isAuthorized = true;
     } catch(err) {
         console.log(err);
