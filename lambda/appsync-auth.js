683cc169829b7886130d6f59e22343343a9d1886
Aaron Kaka <aaron.kaka@gmail.com>
2022-08-23 11:18:00 -0400

    Update appsync-auth.js

--- lambda/appsync-auth.js
+++ lambda/appsync-auth.js
@@ -5,29 +5,31 @@ const client = jwksClient({
     cache: true, // Default Value
     cacheMaxEntries: 32,
     jwksUri: process.env.JWKS_URI + 'jwks.json',
-    requestHeaders: {}, // Optional
     timeout: 30000 // Defaults to 30s
 });
-
 const verificationOptions = {
     "algorithms": "RS512"
 }
 
 exports.handler = async (event) => {
     const userToken = event.authorizationToken || '';
+    // begin parsing the authorization token
     const header = userToken.split(".");
     // the secret will be decoded using base64 and the token verification will use the original random bytes
     const jwtHead = Buffer.from(header[0], "base64").toString();
+    // get reference to signing key
     const kid = JSON.parse(jwtHead).kid;
-
     const key = await client.getSigningKey(kid);
     const signingKey = key.getPublicKey();
+    const jwtPayload = Buffer.from(header[1], "base64").toString();
+    // construct default response
     const response = {
         isAuthorized: false,
-        resolverContext: JSON.parse(Buffer.from(header[1], "base64").toString())
+        resolverContext: JSON.parse(jwtPayload)
     };
     
     try {
+        // verify() will evaluate if the token is expired
         let decoded = jwt.verify(userToken, signingKey, verificationOptions);
         response.isAuthorized = true;
     } catch(err) {
